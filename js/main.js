f22a310b4edb7665125d0b7db5c9ff82b24f193d
jonasrht <info@jonasrht.de>
2021-01-22 10:48:43 +0100

    1

--- js/main.js
+++ js/main.js
@@ -1,29 +1,38 @@
-function resizeApp ()
-{
+var vid = document.getElementById("musicplayer");
 
+function audioVolume() {
+    vid.volume = 1;
 }
 
-function runApp ()
-{
-    'use strict';
-
-    //Init the Phaser game app
-    let app = new App();
-    app.start();
-
-    // Scale to device
+var html5_audiotypes = { //define list of audio file extensions and their associated audio types. Add to it if your specified audio file isn't on this list:
+    "mp3": "audio/mpeg",
+    "mp4": "audio/mp4",
+    "ogg": "audio/ogg",
+    "wav": "audio/wav"
 }
 
-window.onload = function()
-{
-    'use strict';
-    
-    // Check ES6
-    try {
-        
-    } catch (error) {
-        
+function createsoundbite(sound) {
+    var html5audio = document.createElement('audio')
+    if (html5audio.canPlayType) { //check support for HTML5 audio
+        for (var i = 0; i < arguments.length; i++) {
+            var sourceel = document.createElement('source')
+            sourceel.setAttribute('src', arguments[i])
+            if (arguments[i].match(/\.(\w+)$/i))
+                sourceel.setAttribute('type', html5_audiotypes[RegExp.$1])
+            html5audio.appendChild(sourceel)
+        }
+        html5audio.load()
+        html5audio.playclip = function () {
+            html5audio.pause()
+            html5audio.volume = 0.05
+            html5audio.currentTime = 0
+            html5audio.play()
+        }
+        return html5audio
     }
-
-    runApp();
+    else {
+        return { playclip: function () { throw new Error("Your browser doesn't support HTML5 audio unfortunately") } }
+    }
 }
+var mouseoversound = createsoundbite("assetes/audio/button-hover.wav")
+var clicksound = createsoundbite("assetes/audio/button-hover.wav")
