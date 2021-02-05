a92692e7470d391a27d6bdc5eb560bffd34bb3e7
jonasrht <info@jonasrht.de>
2021-02-05 15:15:37 +0100

    game replaced with iframe object

--- lights.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-    let cnv = createCanvas(1278, 1024, WEBGL);
-    cnv.parent('canvas1');
-}
-function draw() {
-    //console.log("Halu");
-    background(220);
-    ellipse(50, 50, 80, 80);
-}
